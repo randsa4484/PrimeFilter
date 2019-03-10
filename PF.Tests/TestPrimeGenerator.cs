26b57f5c5428014ed3a8f966130f174817bcdba8
randsa4484 <andrew.rands@gmail.com>
2019-03-10 16:27:58 +0000

    Added Sieve of Eratosthenes implementation

--- PF.Tests/TestPrimeGenerator.cs
+++ PF.Tests/TestPrimeGenerator.cs
@@ -1,5 +1,7 @@
 
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PF.WebAPI.Services;
 
 namespace PF.Tests
 {
@@ -7,9 +9,29 @@ namespace PF.Tests
     public class TestPrimeGenerator
     {
         [TestMethod]
-        public void Go()
+        public void TestZeroNotPrime()
         {
-            Assert.IsTrue(true);
+            var pg = new PrimesGenerator();
+            var res = pg.GetPrimes(0);
+
+            Assert.IsTrue(res.Count() == 0);
+        }   
+        
+        [TestMethod]
+        public void TestOneIsPrime()
+        {
+            var pg = new PrimesGenerator();
+            var res = pg.GetPrimes(1);
+
+            Assert.IsTrue(res.Count() == 1);
+            Assert.IsTrue(res.First() == 1);
+        } 
+        
+        [TestMethod]
+        public void TestPrimes()
+        {
+            var pg = new PrimesGenerator();
+            var res = pg.GetPrimes(5);
         }
     }
 }
