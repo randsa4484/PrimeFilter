45b1c3b0f1191fe1fdea9d8a4f0ee64151d7018a
randsa66 <andrew_rands@scientia.com>
2019-03-12 23:04:30 +0000

    filtering uses parallel tasks

--- PF.WebAPI/Services/Filtering/PrimesFilter.cs
+++ PF.WebAPI/Services/Filtering/PrimesFilter.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 
@@ -18,28 +19,30 @@ namespace PF.WebAPI.Services.Filtering
 
         public async Task<List<string>> Filter(IEnumerable<string> words)
         {
-            var newWords = new List<string>();
+            var tasks = words.Select(word => TestWord(word)).ToList();
 
-            foreach (var word in words)
+            await Task.WhenAll(tasks.ToArray());
+
+            return tasks.Where(t => t.Result.Value).Select(task => task.Result.Key).ToList();
+        }
+
+        private async Task<KeyValuePair<string, bool>> TestWord(string word)
+        {
+            bool addWord = true;
+            if (double.TryParse(word, out var result))
             {
-                bool addWord = true;
-                if (double.TryParse(word, out var result))
+                if (result > _primeTester.MaxValueSupported)
                 {
-                    if (result > _primeTester.MaxValueSupported)
-                    {
-                        var message = $"Number to be tested ({result}) exceeds the capability of the current prime number tester ({_primeTester.MaxValueSupported})";
-                        _logger.LogError(message);
-                        throw new NumberExceedsPrimeSearchBoundsException(message, result, _primeTester.MaxValueSupported);
-                    }
-
-                    addWord = ! await _primeTester.NumberIsPrime(result);
+                    var message =
+                        $"Number to be tested ({result}) exceeds the capability of the current prime number tester ({_primeTester.MaxValueSupported})";
+                    _logger.LogError(message);
+                    throw new NumberExceedsPrimeSearchBoundsException(message, result, _primeTester.MaxValueSupported);
                 }
 
-                if (addWord)
-                    newWords.Add(word);
+                addWord = !await _primeTester.NumberIsPrime(result);
             }
 
-            return newWords;
+            return new KeyValuePair<string, bool>(word, addWord);
         }
 
         public string Name => "Remove Prime Number Filter";
